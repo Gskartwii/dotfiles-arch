c973688939eece7ca7d3f43c758c53e10f9e6074
Aleksi Hannula <ahannula4@gmail.com>
2021-01-31 14:43:54 +0200

    Init from yatagarasu

---
+++ make-links.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TARGET=${1:-$HOME/.config}
+WD=$(pwd)
+for dir in ./config/*/; do
+    dir=${dir%*/}
+    dir=${dir##*/}
+    [[ -L "$TARGET/$dir" ]] && rm "$TARGET/$dir"
+    ln -s $WD/config/$dir $TARGET/$dir
+done
+for ti in ./.terminfo-italic-fix/*.ti; do
+    tic $ti
+done
+
+[[ ! -d $TARGET/kak/plugins ]] && mkdir $TARGET/kak/plugins
