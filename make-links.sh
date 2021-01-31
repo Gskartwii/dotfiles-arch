5054330cc52cc7ac6c2a847dbcbc0b63a399ea1d
Aleksi Hannula <ahannula4@gmail.com>
2021-01-31 15:24:55 +0200

    Automate zsh dotfile setup

--- make-links.sh
+++ make-links.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-TARGET=${1:-$HOME/.config}
+TARGET=${1:-$HOME}/.config
 WD=$(pwd)
 for dir in ./config/*/; do
     dir=${dir%*/}
     dir=${dir##*/}
     [[ -L "$TARGET/$dir" ]] && rm "$TARGET/$dir"
     ln -s $WD/config/$dir $TARGET/$dir
+    [[ -x "$TARGET/$dir/setup" ]] && $TARGET/$dir/setup "$@"
 done
+
 for ti in ./.terminfo-italic-fix/*.ti; do
     tic $ti
 done
