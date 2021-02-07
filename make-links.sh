1d9c3f17c52e8d632c010f183e26337c578744c8
Aleksi Hannula <ahannula4@gmail.com>
2021-02-07 12:26:40 +0200

    Switch things around for a light theme

--- make-links.sh
+++ make-links.sh
@@ -8,6 +8,7 @@ for dir in ./config/*/; do
     dir=${dir%*/}
     dir=${dir##*/}
     [[ -L "$TARGET/$dir" ]] && rm "$TARGET/$dir"
+    [[ -e "$TARGET/$dir" ]] && echo "Skipping $TARGET/$dir"
     ln -s $WD/config/$dir $TARGET/$dir
     [[ -x "$TARGET/$dir/setup" ]] && $TARGET/$dir/setup "$@"
 done
