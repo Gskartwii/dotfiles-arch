1665b1399552b620262f7b13e379d235ac58f5d8
Aleksi Hannula <ahannula4@gmail.com>
2021-11-26 11:29:05 +0200

    Reorganise environment variables according to their use

--- usr-bin/sway-run.sh
+++ usr-bin/sway-run.sh
@@ -4,11 +4,15 @@ export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
 export XDG_CURRENT_DESKTOP=sway
 
+export WLR_NO_HARDWARE_CURSORS=1
 export MOZ_ENABLE_WAYLAND=1
+export MOZ_DBUS_REMOTE=1
 export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_OPTIONS='-Dawt.userSystemAAFontSettings=lcd'
+export QT_QPA_PLATFORM=wayland
 
 if [[ -x $HOME/.config/sway/env.sh ]]; then
     source $HOME/.config/sway/env.sh
 fi
 
-systemd-cat --identifier=sway sway $@
+systemd-cat --identifier=sway sway --my-next-gpu-wont-be-nvidia $@
